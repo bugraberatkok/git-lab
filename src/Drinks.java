0a9ab1a6ab09ce103accd5d6c1da6a25b580e8de
Buğra Berat Kök <alu.210578@usj.es>
2025-11-24 19:36:51 +0100

    add: new drink into menu

--- src/Drinks.java
+++ src/Drinks.java
@@ -2,6 +2,7 @@ public class Drinks {
     public static void display() {
         // Initial beverages - Students will add more below
         System.out.println("Coffee - $2.50");
-        System.out.println("Orange Juice - $3.00");
+        System.out.println("Ayran - $3.50");
+	System.out.println("Orange Juice - $3.00");
     }
 }
